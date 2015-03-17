2f66d3e261a55a43cba6fce68f0321eb40d2b9e5
Nate Jones <jonesnl@umich.edu>
2015-03-17 01:08:15 -0400

    Remove commented out code, add options menu

--- Android/app/src/main/java/com/jonesnl/bleesandroid/MainActivity.java
+++ Android/app/src/main/java/com/jonesnl/bleesandroid/MainActivity.java
@@ -6,13 +6,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ExpandableListAdapter;
-import android.widget.ExpandableListView;
 import android.widget.SimpleExpandableListAdapter;
-
-//import com.jonesnl.bleesandroid.Classes.ExpandListChild;
-//import com.jonesnl.bleesandroid.R;
-//import com.jonesnl.bleesandroid.Adapter.ExpandListAdapter;
-//import com.jonesnl.bleesandroid.Classes.ExpandListGroup;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -62,69 +56,6 @@ public class MainActivity extends ExpandableListActivity {
         );
         setListAdapter(mAdapter);
     }
-}
-
-
-/*public class MainActivity extends ActionBarActivity {
-
-    private ExpandListAdapter ExpAdapter;
-    private ArrayList<ExpandListGroup> ExpListItems;
-    private ExpandableListView ExpandList;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ExpandList = (ExpandableListView) findViewById(R.id.ExpList);
-        ExpListItems = SetStandardGroups();
-        ExpAdapter = new ExpandListAdapter(MainActivity.this, ExpListItems);
-        ExpandList.setAdapter(ExpAdapter);
-    }
-
-    public ArrayList<ExpandListGroup> SetStandardGroups() {
-        ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
-        ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
-        ExpandListGroup gru1 = new ExpandListGroup();
-        gru1.setName("Comedy");
-        ExpandListChild ch1_1 = new ExpandListChild();
-        ch1_1.setName("A movie");
-        ch1_1.setTag(null);
-
-        list2.add(ch1_1);
-        ExpandListChild ch1_2 = new ExpandListChild();
-        ch1_2.setName("Annother movie");
-        ch1_2.setTag(null);
-        list2.add(ch1_2);
-
-        ExpandListChild ch1_3 = new ExpandListChild();
-        ch1_3.setName("And another movie");
-        ch1_3.setTag(null);
-        list2.add(ch1_3);
-        gru1.setItems(list2);
-        list2 = new ArrayList<ExpandListChild>();
-
-        ExpandListGroup gru2 = new ExpandListGroup();
-        gru2.setName("Action");
-        ExpandListChild ch2_1 = new ExpandListChild();
-        ch2_1.setName("A movie");
-        ch2_1.setTag(null);
-        list2.add(ch2_1);
-        ExpandListChild ch2_2 = new ExpandListChild();
-        ch2_2.setName("An other movie");
-        ch2_2.setTag(null);
-        list2.add(ch2_2);
-        ExpandListChild ch2_3 = new ExpandListChild();
-        ch2_3.setName("And an other movie");
-        ch2_3.setTag(null);
-        list2.add(ch2_3);
-        gru2.setItems(list2);
-        list.add(gru1);
-        list.add(gru2);
-
-        return list;
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -147,4 +78,4 @@ public class MainActivity extends ExpandableListActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}*/
+}
