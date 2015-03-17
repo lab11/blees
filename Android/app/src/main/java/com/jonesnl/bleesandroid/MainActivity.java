a05e1c82dcccd084f5af0534c11a104236cfe237
Nate Jones <jonesnl@umich.edu>
2015-03-17 01:01:44 -0400

    Add collapsable list to android project

--- Android/app/src/main/java/com/jonesnl/bleesandroid/MainActivity.java
+++ Android/app/src/main/java/com/jonesnl/bleesandroid/MainActivity.java
@@ -1,17 +1,128 @@
 package com.jonesnl.bleesandroid;
 
+import android.app.ExpandableListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
+
+//import com.jonesnl.bleesandroid.Classes.ExpandListChild;
+//import com.jonesnl.bleesandroid.R;
+//import com.jonesnl.bleesandroid.Adapter.ExpandListAdapter;
+//import com.jonesnl.bleesandroid.Classes.ExpandListGroup;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ExpandableListActivity {
+    private static final String NAME = "NAME";
+    private static final String IS_EVEN = "IS_EVEN";
+
+    private ExpandableListAdapter mAdapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        List<Map<String, String>> groupData = new ArrayList<Map<String, String>>();
+        List<List<Map<String, String>>> childData = new ArrayList<List<Map<String, String>>>();
+        for (int i = 0; i < 10; i++) {
+            Map<String, String> curGroupMap = new HashMap<String, String>();
+            groupData.add(curGroupMap);
+            curGroupMap.put(NAME, "Group " + i);
+            curGroupMap.put(IS_EVEN, (i % 2 == 0) ? "This group is even" : "This group is odd");
+
+            List<Map<String, String>> children = new ArrayList<Map<String, String>>();
+            for (int j = 0; j < 3; j++) {
+                Map<String, String> curChildMap = new HashMap<String, String>();
+                children.add(curChildMap);
+                curChildMap.put(NAME, "Child " + j);
+                curChildMap.put(IS_EVEN, (j % 2 == 0) ? "This child is even" : "This child is odd");
+            }
+            childData.add(children);
+        }
+
+        // Set up our adapter
+        mAdapter = new SimpleExpandableListAdapter(
+                this,
+                groupData,
+                android.R.layout.simple_expandable_list_item_1,
+                new String[] { NAME, IS_EVEN },
+                new int[] { android.R.id.text1, android.R.id.text2 },
+                childData,
+                android.R.layout.simple_expandable_list_item_2,
+                new String[] { NAME, IS_EVEN },
+                new int[] { android.R.id.text1, android.R.id.text2 }
+        );
+        setListAdapter(mAdapter);
+    }
+}
+
+
+/*public class MainActivity extends ActionBarActivity {
+
+    private ExpandListAdapter ExpAdapter;
+    private ArrayList<ExpandListGroup> ExpListItems;
+    private ExpandableListView ExpandList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ExpandList = (ExpandableListView) findViewById(R.id.ExpList);
+        ExpListItems = SetStandardGroups();
+        ExpAdapter = new ExpandListAdapter(MainActivity.this, ExpListItems);
+        ExpandList.setAdapter(ExpAdapter);
+    }
+
+    public ArrayList<ExpandListGroup> SetStandardGroups() {
+        ArrayList<ExpandListGroup> list = new ArrayList<ExpandListGroup>();
+        ArrayList<ExpandListChild> list2 = new ArrayList<ExpandListChild>();
+        ExpandListGroup gru1 = new ExpandListGroup();
+        gru1.setName("Comedy");
+        ExpandListChild ch1_1 = new ExpandListChild();
+        ch1_1.setName("A movie");
+        ch1_1.setTag(null);
+
+        list2.add(ch1_1);
+        ExpandListChild ch1_2 = new ExpandListChild();
+        ch1_2.setName("Annother movie");
+        ch1_2.setTag(null);
+        list2.add(ch1_2);
+
+        ExpandListChild ch1_3 = new ExpandListChild();
+        ch1_3.setName("And another movie");
+        ch1_3.setTag(null);
+        list2.add(ch1_3);
+        gru1.setItems(list2);
+        list2 = new ArrayList<ExpandListChild>();
+
+        ExpandListGroup gru2 = new ExpandListGroup();
+        gru2.setName("Action");
+        ExpandListChild ch2_1 = new ExpandListChild();
+        ch2_1.setName("A movie");
+        ch2_1.setTag(null);
+        list2.add(ch2_1);
+        ExpandListChild ch2_2 = new ExpandListChild();
+        ch2_2.setName("An other movie");
+        ch2_2.setTag(null);
+        list2.add(ch2_2);
+        ExpandListChild ch2_3 = new ExpandListChild();
+        ch2_3.setName("And an other movie");
+        ch2_3.setTag(null);
+        list2.add(ch2_3);
+        gru2.setItems(list2);
+        list.add(gru1);
+        list.add(gru2);
+
+        return list;
     }
 
 
@@ -36,4 +147,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}*/
