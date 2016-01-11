65c0e0e2ea9664aba6947112f33597c3f8c3b991
Brad Campbell <bradjc5@gmail.com>
2016-01-10 19:48:15 -0500

    update parse to match

--- summon/blees-demo/parse.js
+++ summon/blees-demo/parse.js
@@ -3,34 +3,42 @@
 var parse_advertisement = function (advertisement, cb) {
 
     if (advertisement.localName === 'BLEES') {
-        var service_uuid = advertisement.serviceData[0].uuid;
-        var service_data = advertisement.serviceData[0].data;
+        if ('manufacturerData' in advertisement) {
+            // Need at least 3 bytes. Two for manufacturer identifier and
+            // one for the service ID.
+            if (advertisement.manufacturerData.length >= 3) {
+                // Check that manufacturer ID and service byte are correct
+                var manufacturer_id = advertisement.manufacturerData.readUIntLE(0, 2);
+                var service_id = advertisement.manufacturerData.readUInt8(2);
+                if (manufacturer_id == 0x02E0 && service_id == 0x12) {
+                    // OK! This looks like a BLEES packet
+                    if (advertisement.manufacturerData.length == 14) {
+                        var sensor_data = advertisement.manufacturerData.slice(3);
 
-        // BLE ESS Service
-        if (parseInt(service_uuid, 16) == 0x181A) {
-            // service data is environmental data collected by BLEES
+                        var pressure = sensor_data.readUIntLE(0,4)/10;
+                        var humidity = sensor_data.readUIntLE(4,2)/100;
+                        var temp     = sensor_data.readUIntLE(6,2)/100;
+                        var light    = sensor_data.readUIntLE(8,2);
+                        var accel    = sensor_data.readUIntLE(10,1);
 
-            var pressure = service_data.readUIntLE(0,4)/10;
-            var humidity = service_data.readUIntLE(4,2)/100;
-            var temp     = service_data.readUIntLE(6,2)/100;
-            var light    = service_data.readUIntLE(8,2);
-            var accel    = service_data.readUIntLE(10,1);
+                        var imm_accel = ((accel & 0xF0) != 0);
+                        var min_accel = ((accel & 0x0F) != 0);
 
-            var imm_accel = ((accel & 0xF0) != 0);
-            var min_accel = ((accel & 0x0F) != 0);
+                        var out = {
+                            device: 'BLEES',
+                            pressure_pascals: pressure,
+                            humidity_percent: humidity,
+                            temperature_celcius: temp,
+                            light_lux: light,
+                            acceleration_advertisement: imm_accel,
+                            acceleration_interval: min_accel
+                        };
 
-            var out = {
-                device: 'BLEES',
-                pressure_pascals: pressure,
-                humidity_percent: humidity,
-                temperature_celcius: temp,
-                light_lux: light,
-                acceleration_advertisement: imm_accel,
-                acceleration_interval: min_accel
-            };
-
-            cb(out);
-            return;
+                        cb(out);
+                        return;
+                    }
+                }
+            }
         }
     }
 
