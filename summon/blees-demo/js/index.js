b05eab18407d42a7e2027d54feae51753d418de2
Brad Campbell <bradjc5@gmail.com>
2016-01-10 23:54:59 -0500

    may actaully be getting somewhere

--- summon/blees-demo/js/index.js
+++ summon/blees-demo/js/index.js
@@ -711,50 +711,61 @@ var app = {
         console.log(typeof device.advertising);
         //Parse Advertised Data
         var advertisement = adv_bytes_to_noble_object(device.advertising);
-        console.log(advertisement);
-        var adData = new Uint8Array(device.advertising);
+        console.log(JSON.stringify(advertisement));
 
-        if ((adData[12] != 0x1A) || (adData[13] != 0x18)){
-            app.log("not right");
-            app.onEnable();
-            return;
-        }
+        // Check this is something we can parse
+        if (advertisement.localName == 'BLEES' &&
+            advertisement.serviceUuids.indexOf('181a') !== -1) {
 
-        else{
-            ble.stopScan(app.onStopScan, app.onError);
-        }
+            console.log('cool with this packet')
 
-        // Save when we got this.
-        last_update = Date.now();
+            var mandata = new Uint8Array(advertisement.manufacturerData);
 
-        //app.log("Parsing advertised data...");
-        var pressure_pascals = (( (adData[17] * 16777216) + (adData[16] * 65536 ) + (adData[15] * 256) + adData[14] )/10);
-        var pressure_mmHg = (pressure_pascals*0.007500616827042).toFixed(2);
-        var pressure_atm = (pressure_pascals*0.00000986923266716).toFixed(4);
-        var pressureOut = pressure_mmHg + ' mmHg<br />' + pressure_atm + ' atm';
-        app.log( "Pressure: " + pressureOut);
-        document.getElementById("presVal").innerHTML = String(pressureOut);
 
-        var humidityOut = (( (adData[19] * 256) + adData[18] )/100) + String.fromCharCode(37);
-        app.log( "Humidity: " + humidityOut);
-        document.getElementById("humVal").innerHTML = String(humidityOut);
 
-        var temp_celsius = (((adData[21] * 256) + adData[20])/100).toFixed(1);
-        var temp_fahrenheit = ((temp_celsius * (9/5)) + 32).toFixed(1);
-        var temperatureOut = temp_celsius + " " + String.fromCharCode(176) + "C";
-        temperatureOut    += '<br />' + temp_fahrenheit + " " + String.fromCharCode(176) + "F";
-        app.log( "Temperature: " + temperatureOut);
-        document.getElementById("tempVal").innerHTML = String(temperatureOut);
+        // var adData = new Uint8Array(device.advertising);
 
-        var luxOut = ( (adData[23] * 256) + adData[22]) + " lux" ;
-        app.log( "Lux: " + luxOut);
-        document.getElementById("luxVal").innerHTML = String(luxOut);
+        // if ((adData[12] != 0x1A) || (adData[13] != 0x18)){
+        //     app.log("not right");
+        //     app.onEnable();
+        //     return;
+        // }
 
-        var accdata = adData[24];
-        var immAcc = ((accdata & 17) >> 4);
-        var intAcc = (accdata & 1);
-        app.log("Immediate Acceleration: " + ((accdata & 17) >> 4) );
-        app.log("Interval Acceleration: " + (accdata & 1) );
+        // else{
+        //     ble.stopScan(app.onStopScan, app.onError);
+        // }
+
+            // Save when we got this.
+            last_update = Date.now();
+
+            //app.log("Parsing advertised data...");
+            var pressure_pascals = (( (adData[6] * 16777216) + (adData[5] * 65536 ) + (adData[4] * 256) + adData[3] )/10);
+            var pressure_mmHg = (pressure_pascals*0.007500616827042).toFixed(2);
+            var pressure_atm = (pressure_pascals*0.00000986923266716).toFixed(4);
+            var pressureOut = pressure_mmHg + ' mmHg<br />' + pressure_atm + ' atm';
+            app.log( "Pressure: " + pressureOut);
+            document.getElementById("presVal").innerHTML = String(pressureOut);
+
+            var humidityOut = (( (adData[8] * 256) + adData[7] )/100) + String.fromCharCode(37);
+            app.log( "Humidity: " + humidityOut);
+            document.getElementById("humVal").innerHTML = String(humidityOut);
+
+            var temp_celsius = (((adData[10] * 256) + adData[9])/100).toFixed(1);
+            var temp_fahrenheit = ((temp_celsius * (9/5)) + 32).toFixed(1);
+            var temperatureOut = temp_celsius + " " + String.fromCharCode(176) + "C";
+            temperatureOut    += '<br />' + temp_fahrenheit + " " + String.fromCharCode(176) + "F";
+            app.log( "Temperature: " + temperatureOut);
+            document.getElementById("tempVal").innerHTML = String(temperatureOut);
+
+            var luxOut = ( (adData[12] * 256) + adData[11]) + " lux" ;
+            app.log( "Lux: " + luxOut);
+            document.getElementById("luxVal").innerHTML = String(luxOut);
+
+            var accdata = adData[13];
+            var immAcc = ((accdata & 17) >> 4);
+            var intAcc = (accdata & 1);
+            app.log("Immediate Acceleration: " + ((accdata & 17) >> 4) );
+            app.log("Interval Acceleration: " + (accdata & 1) );
 
 
         // if (immAcc) {
@@ -769,24 +780,28 @@ var app = {
         // }
 
 
-        if (intAcc) {
-            document.getElementById('accLastIntCell').style.color = "#ED97B9";
-            document.getElementById('accLastIntCell2').style.color = "#ED97B9";
-            document.getElementById('accSpinnerInt').style.visibility = "visible";
-            document.getElementById('accNotSpinnerInt').style.visibility = "hidden";
+            if (intAcc) {
+                document.getElementById('accLastIntCell').style.color = "#ED97B9";
+                document.getElementById('accLastIntCell2').style.color = "#ED97B9";
+                document.getElementById('accSpinnerInt').style.visibility = "visible";
+                document.getElementById('accNotSpinnerInt').style.visibility = "hidden";
 
+            } else {
+                document.getElementById('accLastIntCell').style.color = "black";
+                document.getElementById('accLastIntCell2').style.color = "black";
+                document.getElementById('accSpinnerInt').style.visibility = "hidden";
+                document.getElementById('accNotSpinnerInt').style.visibility = "visible";
+            }
+
+            app.update_time_ago();
+            // document.querySelector("#time_stamp").innerHTML = (new Date()).toLocaleTimeString();
+
+            // if(steadyscan_on){
+            //     app.onEnable();
+            // }
         } else {
-            document.getElementById('accLastIntCell').style.color = "black";
-            document.getElementById('accLastIntCell2').style.color = "black";
-            document.getElementById('accSpinnerInt').style.visibility = "hidden";
-            document.getElementById('accNotSpinnerInt').style.visibility = "visible";
-        }
-
-        app.update_time_ago();
-        // document.querySelector("#time_stamp").innerHTML = (new Date()).toLocaleTimeString();
-
-        if(steadyscan_on){
-            app.onEnable();
+            // Not a BLEES packet...
+            app.log('Advertisement was not BLEES.');
         }
 
     },
