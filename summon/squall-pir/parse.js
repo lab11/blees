9871eb6d582abea458fd56ec517bc1d812ec536a
Brad Campbell <bradjc5@gmail.com>
2016-03-04 20:37:54 -0500

    update parse.js to new name

--- summon/squall-pir/parse.js
+++ summon/squall-pir/parse.js
@@ -20,7 +20,7 @@ var parse_advertisement = function (advertisement, cb) {
                         var motion_last_minute    = pir.readUInt8(2); // 1 if the PIR detected motion at any point in the last minute
 
                         var out = {
-                            device: 'SquallPIR',
+                            device: 'Blink',
                             current_motion:        current_motion==1,
                             motion_since_last_adv: motion_since_last_adv==1,
                             motion_last_minute:    motion_last_minute==1
