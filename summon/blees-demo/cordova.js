0549e9d06ae4e1a22202cd86d3ead4625eb9d9e9
Branden Ghena <brghena@umich.edu>
2016-03-24 15:35:15 -0400

    Handle cordova loading errors

--- summon/blees-demo/cordova.js
+++ summon/blees-demo/cordova.js
@@ -2,24 +2,27 @@
 if (navigator.platform.startsWith("iP")) {
     // iOS (iPhone, iPad, iPod)
     console.log("Loading iOS-specific cordova");
-    $.getScript('cordova_ios.js', load_after);
+    $.getScript('cordova_ios.js', load_after)
+        .fail(function(jqxhr, settings, exception) {
+            console.log("Failed to load");
+            console.log("Exception: " + exception);
+            console.log("Attempting to power through");
+            load_after();
+        });
 } else {
     // android or bust
     console.log("Loading android-specific cordova");
     $.getScript('cordova_android.js', load_after)
         .fail(function(jqxhr, settings, exception) {
             console.log("Failed to load");
-            console.log(JSON.stringify(jqxhr));
-            console.log("Settings: " + settings);
             console.log("Exception: " + exception);
+            console.log("Attempting to power through");
+            load_after();
         });
 }
 
 // don't load the user's index.js until cordova is loaded
-function load_after (response, stats) {
-    console.log("Successful load");
-    console.log("Response: " + response);
-    console.log("Status: " + stats);
+function load_after () {
     console.log("Loading user's index.js file");
     $.getScript('js/index.js');
 }
